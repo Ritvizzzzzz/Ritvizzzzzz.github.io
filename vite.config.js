66440f801e7826f7278d7fbf5711a5f0382987c4
Ritviz D'Costa <92784994+Ritvizzzzzz@users.noreply.github.com>
2025-08-07 16:31:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // Add this 'base' property
-  base: '/your-repo-name/',
+  base: 'Ritvizzzzzz.github.io',
   plugins: [react()],
 });
